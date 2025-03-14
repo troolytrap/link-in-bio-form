885abb9da032ad83f30d25006437bc3c74fae0d1
troolytrap <kevinmcdermott@uchicago.edu>
2025-03-14 21:04:36 +0000

    fin

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,16 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+  def display_form
+  render({ :template => "item_templates/form" })
+  end
+  def create_item
+    i = Item.new
+    i.link_url= params.fetch("link")
+    i.link_description= params.fetch("desc")
+    i.thumbnail_url= params.fetch("thumb")
+    i.save
+    
+    redirect_to("/")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
+  get("/backdoor", { :controller => "items", :action => "display_form" })
+  post("/make-a-new-item", {:controller => "items", :action => "create_item"})
   get("/", { :controller => "items", :action => "index" })
 end
